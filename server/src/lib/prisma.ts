2d38d9e0459fe3520e49df755be3ec3bc2e11b4b
Francisco Adilio <franciscoadiliolessa@gmail.com>
2026-06-26 20:58:17 -0300

    chore: installed @libsql/client for prisma client

--- server/src/lib/prisma.ts
+++ server/src/lib/prisma.ts
@@ -1,11 +1,19 @@
 import "dotenv/config";
 import { PrismaClient } from "../../generated/prisma/client.js";
 import { PrismaLibSql } from "@prisma/adapter-libsql";
+import { createClient } from "@libsql/client";
 
-const adapter = new PrismaLibSql({
-  url: `${process.env.TURSO_DATABASE_URL}`, 
-  authToken: `${process.env.TURSO_AUTH_TOKEN}` 
-});
+const url = process.env.TURSO_DATABASE_URL;
+const authToken = process.env.TURSO_AUTH_TOKEN;
+
+if (!url || !authToken) {
+  throw new Error("TURSO_DATABASE_URL and TURSO_AUTH_TOKEN must be defined");
+}
+
+const libsqlClient = createClient({ url, authToken });
+
+const adapter = new PrismaLibSql(libsqlClient as any);
+
 const prisma = new PrismaClient({ adapter });
 
 export { prisma };
